49b7ac6a9f8eda28cd04c9192973f2a82cb1b849
Ramiro Pereira de Magalhães <ramiro.p.magalhaes@gmail.com>
2013-08-21 11:27:38 -0300

    Adding methods to enable the loading of a file that describes the exclusions of the images without needing the file to be in the same folder of the images.

--- markings.cpp
+++ markings.cpp
@@ -3,21 +3,17 @@
 #include <boost/filesystem/fstream.hpp>
 #include <boost/filesystem.hpp>
 
-Markings::Markings()
+Markings::Markings() : empty(new std::vector<Rectangle>())
 {
-    empty = new std::vector<Rectangle>();
 }
 
-Markings::Markings(const std::string base_directory_)
+Markings::~Markings()
 {
-    empty = new std::vector<Rectangle>();
-    setBaseDirectory(base_directory_);
+    reset();
 }
 
-void Markings::setBaseDirectory(const std::string base_directory_)
+void Markings::reset()
 {
-    base_directory = base_directory_;
-
     std::map<std::string, std::vector<Rectangle>*>::iterator it = exclusions.begin();
     const std::map<std::string, std::vector<Rectangle>*>::iterator end = exclusions.end();
     for(; it != end; ++it) {
@@ -26,7 +22,11 @@ void Markings::setBaseDirectory(const std::string base_directory_)
     }
 
     exclusions.clear();
-    load();
+}
+
+void Markings::setBaseDirectory(const std::string base_directory_)
+{
+    base_directory = base_directory_;
 }
 
 std::string Markings::getBaseDirectory() const
@@ -34,18 +34,47 @@ std::string Markings::getBaseDirectory() const
     return base_directory;
 }
 
-void Markings::set(const std::string image, std::vector<Rectangle> *markings)
+bool Markings::loadFilenamesFromBaseDirectory()
+{
+    if ( !boost::filesystem::exists( base_directory ) ) return false;
+
+    boost::filesystem::directory_iterator end_itr; // default construction yields past-the-end
+    for ( boost::filesystem::directory_iterator itr( base_directory ); itr != end_itr; ++itr )
+    {
+        if ( boost::filesystem::is_regular_file(itr->status()) )
+        {
+            const boost::filesystem::path p = itr->path();
+            if ( p.extension() == ".jpg"
+                 || p.extension() == ".png"
+                 || p.extension() == ".bmp"
+                 || p.extension() == ".pgm"
+                 || p.extension() == ".JPG"
+                 || p.extension() == ".PNG"
+                 || p.extension() == ".BMP"
+                 || p.extension() == ".PGM" )
+            {
+                std::vector<Rectangle> * nothingHere = new std::vector<Rectangle>();
+                set(p.filename().string(), nothingHere);
+            }
+        }
+    }
+
+    return false;
+}
+
+std::vector<Rectangle> * Markings::set(const std::string image, std::vector<Rectangle> * markings)
 {
     std::map<std::string, std::vector<Rectangle> *>::iterator it = exclusions.find(image);
 
-    if ( it == exclusions.end() )
+    if ( it != exclusions.end() )
     {
-        exclusions.insert(std::map<std::string, std::vector<Rectangle> *>::value_type(image, markings));
-    }
-    else
-    {
+        std::vector<Rectangle> * returnMe = exclusions[image];
         exclusions[image] = markings;
+        return returnMe;
     }
+
+    exclusions.insert(std::map<std::string, std::vector<Rectangle> *>::value_type(image, markings));
+    return 0;
 }
 
 const std::vector<Rectangle> * Markings::get(const std::string &image) const
@@ -71,15 +100,23 @@ void Markings::remove(const std::string & image)
 
 bool Markings::save()
 {
-    boost::filesystem::path archivePath( base_directory );
+    boost::filesystem::path defaultSave( base_directory );
+    defaultSave = defaultSave / ".jacktool.data";
+    const std::string s = defaultSave.native();
+    return save(s);
+}
 
-    {
+bool Markings::save(const std::string & markingsFile)
+{
+    boost::filesystem::path baseDir( base_directory );
+
+    {//removes images that do not exist in the base_directory
         std::map<std::string, std::vector<Rectangle> *>::iterator it = exclusions.begin();
         const std::map<std::string, std::vector<Rectangle> *>::iterator end = exclusions.end();
-        for(; it != end; ++it)
+        for(; it != end;)
         {
             const std::map<std::string, std::vector<Rectangle> *>::value_type v = *it;
-            const boost::filesystem::path imagePath = archivePath / v.first;
+            const boost::filesystem::path imagePath = baseDir / v.first;
 
             if ( boost::filesystem::exists(imagePath) )
             {
@@ -92,10 +129,8 @@ bool Markings::save()
         }
     }
 
-    archivePath = archivePath / ".jacktool.data";
-
-    boost::filesystem::ofstream outputStream(archivePath, std::ios::out | std::ios::trunc);
-    if (!outputStream.is_open()) //TODO has write permission?
+    boost::filesystem::ofstream outputStream(markingsFile, std::ios::out | std::ios::trunc);
+    if (!outputStream.is_open())
     {
         return false;
     }
@@ -117,11 +152,9 @@ std::map<std::string, std::vector<Rectangle> *>::const_iterator Markings::images
     return exclusions.end();
 }
 
-bool Markings::load()
+bool Markings::load(const std::string & markingsFile)
 {
-    boost::filesystem::path archive( base_directory );
-    archive = archive / ".jacktool.data";
-
+    boost::filesystem::path archive( markingsFile );
     if ( !boost::filesystem::exists(archive) )
     {
         return false;
--- markings.h
+++ markings.h
@@ -16,22 +16,26 @@ class Markings
 {
 public:
     Markings();
-    Markings(const std::string base_directory_);
+    ~Markings();
+
+    void reset();
 
     void setBaseDirectory(const std::string base_directory_);
     std::string getBaseDirectory() const;
+    bool loadFilenamesFromBaseDirectory();
 
-    void set(const std::string image, std::vector<Rectangle> * markings);
+    std::vector<Rectangle> * set(const std::string image, std::vector<Rectangle> * markings);
     const std::vector<Rectangle> * get(const std::string & image) const;
     void remove(const std::string & image);
+
     std::map<std::string, std::vector<Rectangle> *>::const_iterator imagesExclusionsMapBegin() const;
     std::map<std::string, std::vector<Rectangle> *>::const_iterator imagesExclusionsMapEnd() const;
 
     bool save();
+    bool save(const std::string &markingsFile);
+    bool load(const std::string & markingsFile);
 
 private:
-    bool load();
-
     template<class Archive> void serialize(Archive & ar, const unsigned int);
     friend class boost::serialization::access;
 
